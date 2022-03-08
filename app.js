53ce2b2b5dd043db28935c61ba964fb3430345ff
catscansmelldogs <cyams.1995@gmail.com>
2022-03-08 08:53:07 +0800

    after one class finished

---
+++ app.js
@@ -0,0 +1,39 @@
+/** @format */
+
+const sections = document.querySelectorAll("section");
+const bubble = document.querySelector(".bubble");
+const gradients = [
+  "linear-gradient(90deg, #1f005c, #5b0060, #870160, #ac255e, #ca485c, #e16b5c, #f39060, #ffb56b)",
+  "linear-gradient(to right, #8a2387, #e94057, #f27121)",
+  "linear-gradient(90deg, #1f005c, #5b0060, #870160, #ac255e, #ca485c, #e16b5c, #f39060, #ffb56b)",
+];
+
+const options = {
+  threshold: 0.7,
+};
+
+let observer = new IntersectionObserver(navCheck, options);
+
+function navCheck(entries) {
+  entries.forEach((entry) => {
+    const className = entry.target.className;
+    const activeAnchor = document.querySelector(`[data-page=${className}]`);
+    const gradientIndex = entry.target.getAttribute("data-index");
+    const coords = activeAnchor.getBoundingClientRect();
+    const directions = {
+      height: coords.height,
+      width: coords.width,
+      top: coords.top,
+      left: coords.left,
+    };
+    if (entry.isIntersecting) {
+      bubble.style.setProperty("left", `${directions.left}px`);
+      bubble.style.setProperty("top", `${directions.top}px`);
+      bubble.style.setProperty("width", `${directions.width}px`);
+      bubble.style.setProperty("height", `${directions.height}px`);
+    }
+  });
+}
+sections.forEach((section) => {
+  observer.observe(section);
+});
